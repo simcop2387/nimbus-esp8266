c98a839f258038bb5a93fad96dd8da5f163475d2
Ryan Voots <simcop2387@simcop2387.info>
2015-08-30 19:32:37 -0700

    Compile upon upload to save space and performance

--- upload.sh
+++ upload.sh
@@ -4,6 +4,15 @@ LUA_FILES=*.lua
 HTTP_FILES=http/*
 LUATOOL=./luatool.py
 
+echo Wiping...
+$LUATOOL -w
+
 for f in $LUA_FILES $HTTP_FILES; do
-	$LUATOOL -f $f -t ${f/\//_}
+	if [[ ${f: -4} == ".lua" && $f != "init.lua" ]]; then
+		$LUATOOL -c -f $f -t $f
+	else
+		$LUATOOL -f $f -t $f
+        fi
 done
+
+$LUATOOL -l
